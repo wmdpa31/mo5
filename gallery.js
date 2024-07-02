b52a81328cb330ca972f751273e8bf26118cb103
소우주 <sowoojoo@soujuui-MacBookPro.local>
2024-07-02 12:30:29 +0900

    사진수정

--- gallery.js
+++ gallery.js
@@ -48,7 +48,7 @@ window.onload = () => {
       content: {
         title: "심재환 ♥ 안수민의 모바일 청첩장",
         description: "결혼식에 초대합니다♡",
-        imageUrl: "https://i.imgur.com/cy81HnO.png",
+        imageUrl: "https://i.imgur.com/ob3Fe37.png",
         imageWidth: 1200,
         imageHeight: 630,
         link: {
